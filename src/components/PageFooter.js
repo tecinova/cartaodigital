ee02388f28dd6c2c3ece9d558eca1a284ebe841f
Roberto Alves <77143340+RobertoAlves86@users.noreply.github.com>
2021-02-12 10:41:54 -0300

    Update PageFooter.js

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -7,7 +7,7 @@ export default function PageFooter() {
       <ul className="copyright">
         <li>&copy; {config.authorName}</li>
         <li>
-          Design em HTML5 + Unsplash |<a href="">Anubhav</a> {' '}
+          Design em HTML5 + Unsplash @<a href="">Anubhav</a> {' '}
           <a href="">Srivastava</a>
         </li>
       </ul>
