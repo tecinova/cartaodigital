5f04112481b0e4184b6dc7a2e3405c718cf3f2f2
Roberto Alves <77143340+RobertoAlves86@users.noreply.github.com>
2021-02-12 22:21:11 -0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#3B5997',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `https://roberto.tecinovaservicos.com.br/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/gatsby-starter-identity/`, // This path is subpath of your hosting https://domain/portfolio
   authorName: 'Roberto Alves',
   heading: 'Tecnólogo',
   // social
