e3704974f89c3cb9441d99966b34137f63324165
Roberto Alves <77143340+RobertoAlves86@users.noreply.github.com>
2021-08-02 07:35:26 -0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   authorName: 'Roberto Alves',
-  heading: 'Tecnólogo',
+  heading: 'CEO & Founder na TECiNOVA',
   // social
   socialLinks: [
     {
